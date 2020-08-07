c0cc039bf9af0e165c0570863e1fc84f14b246ef
ShotaEbikawa <shotaebikawa@gmail.com>
2020-08-07 03:58:03 -0700

    Added save buttons and pop-up modals to the website

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
 const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
 global.fetch = fetch;
 dotenv.config();
 const Unsplash = require('unsplash-js').default;
@@ -16,6 +17,7 @@ const unsplash = new Unsplash({
 });
 
 app.use(cors());
+app.use(bodyParser.json())
 
 app.get('/getPopularImages', (req,res) => {
     unsplash.photos.listPhotos(1,20,"popular")
@@ -27,24 +29,35 @@ app.get('/getPopularImages', (req,res) => {
 
 app.get('/downloadImage', (req,res) => {
     unsplash.photos.getPhoto("mtNweauBsMQ")
-    .then(toJson)
-    .then(json => {
-      console.log(unsplash.photos.downloadPhoto);
-      res.send({unsplash: unsplash, image: json})
-    });
+        .then(toJson)
+        .then(json => {
+        console.log(unsplash.photos.downloadPhoto);
+        res.send({unsplash: unsplash, image: json})
+        });
 })
 
 app.get('/searchImages', (req,res) => {
     const query = req.query.query;
     console.log(query)
     unsplash.search.photos(query, 1, 20, {orientation: "portrait"})
-    .then(toJson)
-    .then(json => {
-        console.log(json.results)
-        res.send(json.results);
-    })
-    .catch(err => console.log(err));
+        .then(toJson)
+        .then(json => {
+            console.log(json.results)
+            res.send(json.results);
+        })
+        .catch(err => console.log(err));
 })
 
+app.post('/searchCertainImage', (req, res) => {
+    console.log(req);
+    const id = req.body.id;
+    console.log(id);
+    unsplash.photos.getPhoto(id)
+        .then(toJson)
+        .then(json => {
+            console.log(json);
+            res.send(json);
+        });
+})
 
 app.listen(PORT, () => { console.log(`server listening on port ${PORT}`)});
