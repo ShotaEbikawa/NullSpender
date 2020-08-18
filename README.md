91138b0798c14d93f9f2e6ba8094110db3a7821e
ShotaEbikawa <shotaebikawa@gmail.com>
2020-08-18 04:44:50 -0700

    Made an update to the README file

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ download premium stock photographies for free. <br/>
 The website is deployed on Heroku, which can be accessed through this URL: <br/>
 https://nullspender.herokuapp.com
 
+## Prerequisites
+Create a developer account for Unsplash API: <br/>
+https://unsplash.com/join
+
 ## Installation:
 Download Node.js v10.16.0 <br/>
 https://nodejs.org/en/download/
@@ -16,8 +20,16 @@ https://nodejs.org/en/download/
   <pre>cd NullSpender</pre>
 3) Install the dependencies for the project:<br/>
  <pre>npm install</pre>
-4) Run the given script: <br/>
-<pre>npm start</pre>
-5) Navigate to http://localhost:3000
+5) Navigate to the server directory:<br/>
+<pre>cd server</pre>
 
-6) Enjoy the website!
+6) Create an .env file
+
+7) Add given environment variables in the .env file:
+<pre>
+API_KEY = Your Unsplash API key
+SECRET_KEY = Your Unsplash API secret key 
+</pre>
+
+8) Run the given script: <br/>
+<pre>npm start</pre>
--- resources/js/behaviors/ImageAPI.js
+++ resources/js/behaviors/ImageAPI.js
@@ -1,22 +1,21 @@
 export const ImageAPI = (() => {
-    const production = 'https://nullspender.herokuapp.com';
     const development = 'http://localhost:3000'
     const getPopularImages = async () => {
-        const url = `${production}/getPopularImages`
+        const url = `${development}/getPopularImages`
         let images = await fetch(url);
         let imageList = await images.json();
         return imageList;
     }
     
     const getQueriedImages = async (query) => {
-        const url = `${production}/searchImages?query=${query}`
+        const url = `${development}/searchImages?query=${query}`
         let images = await fetch(url);
         let imageList = await images.json();
         return imageList;
     }
     
     const getCertainImage = async (id) => {
-        let url = `${production}/searchCertainImage`
+        let url = `${development}/searchCertainImage`
         let settings = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
